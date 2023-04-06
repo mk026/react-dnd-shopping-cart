19d11a4a5b036794da4ae0241bbf65e35c98cf30
Mikhail <mk0602604@gmail.com>
2023-04-06 23:51:44 +0300

    Add search-products-form component

--- src/components/products/Products.tsx
+++ src/components/products/Products.tsx
@@ -1,10 +1,12 @@
 import { FC } from "react";
 
 import ProductsList from "./products-list";
+import SearchProductsForm from "./search-products-form";
 
 const Products: FC = () => {
   return (
     <div>
+      <SearchProductsForm />
       <ProductsList products={[]} />
     </div>
   );
---
+++ src/components/products/search-products-form/SearchProductsForm.tsx
@@ -0,0 +1,14 @@
+import { FC } from "react";
+
+const SearchProductsForm: FC = () => {
+  return (
+    <form>
+      <label>
+        Search Products
+        <input type="text" />
+      </label>
+    </form>
+  );
+};
+
+export default SearchProductsForm;
