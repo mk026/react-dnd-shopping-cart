76c078892c8320991d6347f2d2895f9b269980d1
Mikhail <mk0602604@gmail.com>
2023-04-13 00:43:07 +0300

    Create cart-button component

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,9 +1,12 @@
 import { FC } from "react";
 
+import CartButton from "./cart-button";
+
 const Header: FC = () => {
   return (
     <div>
       <h1>React dnd shopping cart</h1>
+      <CartButton />
     </div>
   );
 };
---
+++ src/components/header/cart-button/CartButton.tsx
@@ -0,0 +1,7 @@
+import { FC } from "react";
+
+const CartButton: FC = () => {
+  return <button>Cart</button>;
+};
+
+export default CartButton;
