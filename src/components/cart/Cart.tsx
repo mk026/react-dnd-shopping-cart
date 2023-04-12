05f06b58f8f9b62cf4277ed7fd459747c7d4a562
Mikhail <mk0602604@gmail.com>
2023-04-12 23:39:01 +0300

    Create cart-controls component

--- src/components/cart/Cart.tsx
+++ src/components/cart/Cart.tsx
@@ -1,7 +1,14 @@
 import { FC } from "react";
 
+import CartControls from "./cart-controls";
+
 const Cart: FC = () => {
-  return <div>Cart</div>;
+  return (
+    <div>
+      <p>Cart</p>
+      <CartControls />
+    </div>
+  );
 };
 
 export default Cart;
---
+++ src/components/cart/cart-controls/CartControls.tsx
@@ -0,0 +1,7 @@
+import { FC } from "react";
+
+const CartControls: FC = () => {
+  return <div>CartControls</div>;
+};
+
+export default CartControls;
