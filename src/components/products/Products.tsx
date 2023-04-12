236f98c69012d90d304423f932a7acca4a615eda
Mikhail <mk0602604@gmail.com>
2023-04-12 23:39:40 +0300

    Create products-filters component

--- src/components/products/Products.tsx
+++ src/components/products/Products.tsx
@@ -2,11 +2,13 @@ import { FC } from "react";
 
 import ProductsList from "./products-list";
 import SearchProductsForm from "./search-products-form";
+import ProductsFilters from "./products-filters/ProductsFilters";
 
 const Products: FC = () => {
   return (
     <div>
       <SearchProductsForm />
+      <ProductsFilters />
       <ProductsList products={[]} />
     </div>
   );
---
+++ src/components/products/products-filters/ProductsFilters.tsx
@@ -0,0 +1,7 @@
+import { FC } from "react";
+
+const ProductsFilters: FC = () => {
+  return <div>ProductsFilters</div>;
+};
+
+export default ProductsFilters;
