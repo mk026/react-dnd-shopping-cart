af3105c65f3874d1845625436d2581313ea6e32d
Mikhail <mk0602604@gmail.com>
2023-04-14 23:37:09 +0300

    Create cart-total component

--- src/components/cart/Cart.tsx
+++ src/components/cart/Cart.tsx
@@ -1,11 +1,13 @@
 import { FC } from "react";
 
 import CartControls from "./cart-controls";
+import CartTotal from "./cart-total";
 
 const Cart: FC = () => {
   return (
     <div>
       <p>Cart</p>
+      <CartTotal />
       <CartControls />
     </div>
   );
---
+++ src/components/cart/cart-total/CartTotal.tsx
@@ -0,0 +1,7 @@
+import { FC } from "react";
+
+const CartTotal: FC = () => {
+  return <div>CartTotal</div>;
+};
+
+export default CartTotal;
