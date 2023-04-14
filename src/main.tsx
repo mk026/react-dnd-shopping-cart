ceba830946c0fd3d78cc74ba150e7d7a87ba6051
Mikhail <mk0602604@gmail.com>
2023-04-14 23:54:43 +0300

    Setup react-dnd

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
 
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <DndProvider backend={HTML5Backend}>
+      <App />
+    </DndProvider>
   </React.StrictMode>
 );
