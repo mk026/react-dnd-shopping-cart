a410d13928eead8dbdaf1bd628fa55514fbc001e
Mikhail <mk0602604@gmail.com>
2023-05-10 23:39:31 +0300

    Refactor products page

--- src/pages/products-page/ProductsPage.tsx
+++ src/pages/products-page/ProductsPage.tsx
@@ -1,7 +1,15 @@
 import { FC } from "react";
 
+import Products from "../../components/products";
+import PageTitle from "../../components/page-title";
+
 const ProductsPage: FC = () => {
-  return <div>ProductsPage</div>;
+  return (
+    <div>
+      <PageTitle>Products</PageTitle>
+      <Products />
+    </div>
+  );
 };
 
 export default ProductsPage;
