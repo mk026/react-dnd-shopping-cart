411a42f9618db277c69dac9a10468b1a8a5be917
Mikhail <mk0602604@gmail.com>
2023-05-10 23:21:24 +0300

    Add global styles

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,8 @@ import { HTML5Backend } from "react-dnd-html5-backend";
 
 import App from "./App";
 
+import "./styles/global.css";
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <DndProvider backend={HTML5Backend}>
