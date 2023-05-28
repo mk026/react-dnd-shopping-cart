0c0c6d8564eedca37bc1b765b05bae935e17ee74
Mikhail <mk0602604@gmail.com>
2023-05-29 02:32:01 +0300

    Refactor header component

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,14 +1,17 @@
 import { FC } from "react";
+import { AppBar } from "@mui/material";
 
 import CartButton from "./cart-button";
 import AppTitle from "./app-title";
 
+import classes from "./Header.module.css";
+
 const Header: FC = () => {
   return (
-    <header>
+    <AppBar className={classes.header}>
       <AppTitle />
       <CartButton />
-    </header>
+    </AppBar>
   );
 };
 
