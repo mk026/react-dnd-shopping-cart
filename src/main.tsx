9ee99b6c27a9f39a93e2b70004923589e2d2a50c
Mikhail <mk0602604@gmail.com>
2023-05-28 23:18:03 +0300

    Create query-client

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
+import { QueryClientProvider } from "@tanstack/react-query";
 
 import App from "./App";
+import { queryClient } from "./api/queryClient";
 
 import "./styles/global.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <DndProvider backend={HTML5Backend}>
-      <App />
-    </DndProvider>
+    <QueryClientProvider client={queryClient}>
+      <DndProvider backend={HTML5Backend}>
+        <App />
+      </DndProvider>
+    </QueryClientProvider>
   </React.StrictMode>
 );
