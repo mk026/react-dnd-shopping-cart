b9666ba89dc4f96137eea72eaf67c2cffe021573
Mikhail <mk0602604@gmail.com>
2023-05-28 23:30:04 +0300

    Add react-router-dom package

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
 import { QueryClientProvider } from "@tanstack/react-query";
@@ -11,10 +12,12 @@ import "./styles/global.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <DndProvider backend={HTML5Backend}>
-        <App />
-      </DndProvider>
-    </QueryClientProvider>
+    <BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <DndProvider backend={HTML5Backend}>
+          <App />
+        </DndProvider>
+      </QueryClientProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
