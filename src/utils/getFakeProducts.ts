0f62589000e7e6de944a64aaaa6b9d6f0eb3ea36
Mikhail <mk0602604@gmail.com>
2023-05-29 23:37:51 +0300

    Create getFakeProducts util

---
+++ src/utils/getFakeProducts.ts
@@ -0,0 +1,26 @@
+import { IProduct } from "../types/product";
+
+const fakeText =
+  "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum assumenda nulla quaerat ut mollitia non! Veritatis possimus omnis inventore, nemo recusandae vel nostrum voluptate sed deserunt aliquid, nisi minima error!";
+
+export const getFakeProducts = (): IProduct[] => {
+  const fakeProducts: IProduct[] = [];
+  const fakeTextArray = fakeText.split(" ");
+  for (let i = 0; i < 10; i++) {
+    const fakeProduct: IProduct = {
+      id: i,
+      description: fakeText.repeat(Math.ceil(Math.random() * 10)),
+      title: fakeTextArray[
+        Math.floor(Math.random() * fakeTextArray.length)
+      ].replace(
+        /(^\w{1})(\w*)(\W*)/,
+        (_, firstChar: string, characters: string) =>
+          firstChar.toUpperCase().concat(characters)
+      ),
+      imageUrl: "fake_url",
+      price: Math.floor(Math.random() * 100),
+    };
+    fakeProducts.push(fakeProduct);
+  }
+  return fakeProducts;
+};
