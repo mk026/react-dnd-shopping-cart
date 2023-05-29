1de1f1b479ac74d030bd151f0b16b1455d9b4768
Mikhail <mk0602604@gmail.com>
2023-05-29 23:57:50 +0300

    Add home page link in app-title component

--- src/components/header/app-title/AppTitle.tsx
+++ src/components/header/app-title/AppTitle.tsx
@@ -1,11 +1,19 @@
 import { FC } from "react";
+import { Link } from "react-router-dom";
 import { Typography } from "@mui/material";
+
+import { Paths } from "../../../routes";
 
 import classes from "./AppTitle.module.css";
 
 const AppTitle: FC = () => {
   return (
-    <Typography variant="h1" className={classes.title}>
+    <Typography
+      variant="h1"
+      component={Link}
+      to={Paths.HOME}
+      className={classes.title}
+    >
       React dnd shopping cart
     </Typography>
   );
