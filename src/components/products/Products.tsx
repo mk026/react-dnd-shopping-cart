4cea8fe81825e7b2ce1c95b7447a2264658a15b7
Mikhail <mk0602604@gmail.com>
2023-05-29 23:39:04 +0300

    Render products

--- src/components/products/Products.tsx
+++ src/components/products/Products.tsx
@@ -3,13 +3,15 @@ import { FC } from "react";
 import ProductsList from "./products-list";
 import SearchProductsForm from "./search-products-form";
 import ProductsFilters from "./products-filters/ProductsFilters";
+import { getFakeProducts } from "../../utils/getFakeProducts";
 
 const Products: FC = () => {
+  const products = getFakeProducts();
   return (
     <div>
       <SearchProductsForm />
       <ProductsFilters />
-      <ProductsList products={[]} />
+      <ProductsList products={products} />
     </div>
   );
 };
