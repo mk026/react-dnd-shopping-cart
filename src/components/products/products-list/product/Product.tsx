f2e94f01e5f4b60d655efb142ed5cf61d8e98fe4
Mikhail <mk0602604@gmail.com>
2023-05-30 23:46:30 +0300

    Make product items draggable

--- src/components/products/products-list/product/Product.tsx
+++ src/components/products/products-list/product/Product.tsx
@@ -3,14 +3,17 @@ import { FC } from "react";
 import { IProduct } from "../../../../types/product";
 import { Link } from "react-router-dom";
 import { Paths } from "../../../../routes";
+import { useProduct } from "../../../../hooks/useProduct";
 
 interface ProductProps {
   product: IProduct;
 }
 
 const Product: FC<ProductProps> = ({ product }) => {
+  const [, ref] = useProduct(product);
+
   return (
-    <div>
+    <div ref={ref}>
       <Link to={`${Paths.PRODUCTS}/${product.id}`}>{product.title}</Link>
       <p>{product.description}</p>
       <p>{product.price}</p>
