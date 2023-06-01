0550c68862341a845e755cb5a3f2aeb263c4a6fd
Mikhail <mk0602604@gmail.com>
2023-06-01 23:59:00 +0300

    Add styles for app-info component

--- src/components/app-info/AppInfo.tsx
+++ src/components/app-info/AppInfo.tsx
@@ -1,7 +1,16 @@
 import { FC } from "react";
+import { Paper, Typography } from "@mui/material";
+
+import classes from "./AppInfo.module.css";
 
 const AppInfo: FC = () => {
-  return <div>Welcome to React DnD shopping cart demo app</div>;
+  return (
+    <Paper className={classes.container}>
+      <Typography variant="body2" className={classes.info}>
+        Welcome to React DnD shopping cart demo app
+      </Typography>
+    </Paper>
+  );
 };
 
 export default AppInfo;
