c4619417021b0bdae2793f7242d96a5800774719
Mikhail <mk0602604@gmail.com>
2023-06-01 23:58:17 +0300

    Refactor products-grid styles

--- src/components/products/products-grid/ProductsGrid.tsx
+++ src/components/products/products-grid/ProductsGrid.tsx
@@ -4,13 +4,15 @@ import { Grid } from "@mui/material";
 import { IProduct } from "../../../types/product";
 import Product from "./product";
 
+import classes from "./ProductsGrid.module.css";
+
 interface ProductsListProps {
   products: IProduct[];
 }
 
 const ProductsList: FC<ProductsListProps> = ({ products }) => {
   return (
-    <Grid container>
+    <Grid container className={classes.grid}>
       {products.map((product) => (
         <Product key={product.id} product={product} />
       ))}
