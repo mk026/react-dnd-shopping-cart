bb7d06156d766a7d78bb04fc7b5eaee3e648f4c7
Mikhail <mk0602604@gmail.com>
2023-06-01 23:59:29 +0300

    Add styles for view-products-button component

--- src/components/view-products-button/ViewProductsButton.tsx
+++ src/components/view-products-button/ViewProductsButton.tsx
@@ -1,10 +1,17 @@
 import { FC } from "react";
 import { Link } from "react-router-dom";
+import { Button } from "@mui/material";
 
 import { Paths } from "../../routes";
 
+import classes from "./ViewProductsButton.module.css";
+
 const ViewProductsButton: FC = () => {
-  return <Link to={Paths.PRODUCTS}>View Products</Link>;
+  return (
+    <Button component={Link} to={Paths.PRODUCTS} className={classes.button}>
+      View Products
+    </Button>
+  );
 };
 
 export default ViewProductsButton;
