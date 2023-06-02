922a4d2c39d943376d044f85f70fbc14c4dc56c2
Mikhail <mk0602604@gmail.com>
2023-06-02 23:48:31 +0300

    Add type for app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
+import { FC } from "react";
 import { RouterProvider } from "react-router-dom";
 
 import { router } from "./routes";
 
-const App = () => {
+const App: FC = () => {
   return <RouterProvider router={router} />;
 };
 
