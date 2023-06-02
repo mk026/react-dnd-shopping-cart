cb089f67b87ff7131c25f9144cab6467d7740659
Mikhail <mk0602604@gmail.com>
2023-06-02 23:50:38 +0300

    Refactor notification component markup

--- src/components/notification/Notification.tsx
+++ src/components/notification/Notification.tsx
@@ -1,7 +1,30 @@
 import { FC } from "react";
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  Typography,
+} from "@mui/material";
 
-const Notification: FC = () => {
-  return <div>Notification</div>;
+interface NotificationProps {
+  open: boolean;
+  onClose: () => void;
+}
+
+const Notification: FC<NotificationProps> = ({ open, onClose }) => {
+  return (
+    <Dialog open={open} onClose={onClose}>
+      <DialogTitle>Notification</DialogTitle>
+      <DialogContent>
+        <Typography variant="body2">Success Notification</Typography>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={onClose}>Close</Button>
+      </DialogActions>
+    </Dialog>
+  );
 };
 
 export default Notification;
