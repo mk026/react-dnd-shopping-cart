e9180620c3d18921228503823cb4436fbdb0d301
Mikhail <mk0602604@gmail.com>
2023-06-05 16:52:25 +0300

    Refactor cart component

--- src/components/cart/Cart.tsx
+++ src/components/cart/Cart.tsx
@@ -10,13 +10,17 @@ import classes from "./Cart.module.css";
 const Cart: FC = () => {
   const { isOpen, ref } = useCart();
 
-  return isOpen ? (
+  if (!isOpen) {
+    return null;
+  }
+
+  return (
     <Card className={classes.cart} ref={ref}>
       <p>Cart</p>
       <CartTotal total={0} />
       <CartControls />
     </Card>
-  ) : null;
+  );
 };
 
 export default Cart;
