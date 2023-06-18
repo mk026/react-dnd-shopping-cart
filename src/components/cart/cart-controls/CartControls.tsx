0c76acb349fbbe24037266f6ca537b4bf76371ab
Mikhail <mk0602604@gmail.com>
2023-06-18 23:01:18 +0300

    Refactor cart-controls component
    Use router link component for order button

--- src/components/cart/cart-controls/CartControls.tsx
+++ src/components/cart/cart-controls/CartControls.tsx
@@ -1,14 +1,18 @@
 import { FC } from "react";
+import { Link } from "react-router-dom";
 import { Button, Stack } from "@mui/material";
 
 import { useCartContext } from "../../../hooks/useCartContext";
+import { Paths } from "../../../routes";
 
 const CartControls: FC = () => {
   const { closeCart } = useCartContext();
 
   return (
     <Stack direction="row">
-      <Button>Order</Button>
+      <Button component={Link} to={Paths.ORDER}>
+        Order
+      </Button>
       <Button onClick={closeCart}>Close</Button>
     </Stack>
   );
