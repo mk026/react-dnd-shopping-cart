af9ffce166d10b138c6a8eee302e7b1d5bc1ac26
Mikhail <mk0602604@gmail.com>
2023-06-18 23:11:57 +0300

    Wrap app component in browser-router

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
 import { QueryClientProvider } from "@tanstack/react-query";
@@ -13,14 +14,16 @@ import "./styles/global.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <CartProvider>
-      <StyledEngineProvider injectFirst>
-        <QueryClientProvider client={queryClient}>
-          <DndProvider backend={HTML5Backend}>
-            <App />
-          </DndProvider>
-        </QueryClientProvider>
-      </StyledEngineProvider>
-    </CartProvider>
+    <BrowserRouter>
+      <CartProvider>
+        <StyledEngineProvider injectFirst>
+          <QueryClientProvider client={queryClient}>
+            <DndProvider backend={HTML5Backend}>
+              <App />
+            </DndProvider>
+          </QueryClientProvider>
+        </StyledEngineProvider>
+      </CartProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
