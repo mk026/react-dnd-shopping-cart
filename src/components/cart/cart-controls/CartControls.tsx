e7d55c7eee54dc5120780c1f188dd1a5f76fe9ca
Mikhail <mk0602604@gmail.com>
2023-06-19 23:11:52 +0300

    Add styles for cart-controls component

--- src/components/cart/cart-controls/CartControls.tsx
+++ src/components/cart/cart-controls/CartControls.tsx
@@ -5,15 +5,23 @@ import { Button, Stack } from "@mui/material";
 import { useCartContext } from "../../../hooks/useCartContext";
 import { Paths } from "../../../routes";
 
+import classes from "./CartControls.module.css";
+
 const CartControls: FC = () => {
   const { closeCart } = useCartContext();
 
   return (
-    <Stack direction="row">
-      <Button component={Link} to={Paths.ORDER}>
+    <Stack direction="row" className={classes.controls}>
+      <Button
+        component={Link}
+        to={Paths.ORDER}
+        className={classes["order-btn"]}
+      >
         Order
       </Button>
-      <Button onClick={closeCart}>Close</Button>
+      <Button onClick={closeCart} className={classes["close-btn"]}>
+        Close
+      </Button>
     </Stack>
   );
 };
