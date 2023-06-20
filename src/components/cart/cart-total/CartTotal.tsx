18e310d53555d6c7faba1e5d69b5195d10dd5602
Mikhail <mk0602604@gmail.com>
2023-06-20 23:08:30 +0300

    Add styles for cart-total component

--- src/components/cart/cart-total/CartTotal.tsx
+++ src/components/cart/cart-total/CartTotal.tsx
@@ -1,15 +1,14 @@
 import { FC } from "react";
+import { Typography } from "@mui/material";
+
+import classes from "./CartTotal.module.css";
 
 interface CartTotalProps {
   total: number;
 }
 
 const CartTotal: FC<CartTotalProps> = ({ total }) => {
-  return (
-    <div>
-      <p>Total: {total}</p>
-    </div>
-  );
+  return <Typography className={classes.total}>Total: {total}</Typography>;
 };
 
 export default CartTotal;
