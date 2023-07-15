6476c0e2915652f0393868489512f4b73e678755
Mikhail <mk0602604@gmail.com>
2023-07-15 23:25:23 +0300

    Render product data on product page

--- src/pages/product-page/ProductPage.tsx
+++ src/pages/product-page/ProductPage.tsx
@@ -1,11 +1,22 @@
 import { FC } from "react";
 import { useParams } from "react-router-dom";
+import { Typography } from "@mui/material";
 
 import PageTitle from "../../components/page-title";
+import { useGetProductQuery } from "../../hooks/useGetProductQuery";
 
 const ProductPage: FC = () => {
   const { id } = useParams<"id">();
-  return <PageTitle>ProductPage of product with id {id}</PageTitle>;
+  const { data } = useGetProductQuery(Number(id));
+
+  return (
+    <>
+      <PageTitle>ProductPage of product with id {id}</PageTitle>
+      <Typography>{data?.title}</Typography>
+      <Typography>{data?.description}</Typography>
+      <Typography>{data?.price}</Typography>
+    </>
+  );
 };
 
 export default ProductPage;
