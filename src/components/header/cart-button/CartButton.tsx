71da3038471a25488cd4a974daa1c2b9cbb06095
Mikhail <mk0602604@gmail.com>
2023-07-16 22:50:23 +0300

    Render current cart items count in cart-button

--- src/components/header/cart-button/CartButton.tsx
+++ src/components/header/cart-button/CartButton.tsx
@@ -1,17 +1,19 @@
 import { FC } from "react";
-import { Button } from "@mui/material";
+import { Badge, Button } from "@mui/material";
 
 import { useCartContext } from "../../../hooks/useCartContext";
 
 import classes from "./CartButton.module.css";
 
 const CartButton: FC = () => {
-  const { toggleCart } = useCartContext();
+  const { toggleCart, items } = useCartContext();
 
   return (
-    <Button className={classes.button} onClick={toggleCart}>
-      Cart
-    </Button>
+    <Badge badgeContent={items.length}>
+      <Button className={classes.button} onClick={toggleCart}>
+        Cart
+      </Button>
+    </Badge>
   );
 };
 
