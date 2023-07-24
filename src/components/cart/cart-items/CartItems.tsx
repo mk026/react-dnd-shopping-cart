184b0e82c66d84c202d15a3aa6519f20706e9163
Mikhail <mk0602604@gmail.com>
2023-07-24 22:34:40 +0300

    Add styles for cart-items component

--- src/components/cart/cart-items/CartItems.tsx
+++ src/components/cart/cart-items/CartItems.tsx
@@ -4,13 +4,15 @@ import { FC } from "react";
 import CartItem from "./cart-item";
 import { ICartItem } from "../../../types/cart";
 
+import classes from "./CartItems.module.css";
+
 interface CartItemsProps {
   items: ICartItem[];
 }
 
 const CartItems: FC<CartItemsProps> = ({ items }) => {
   return (
-    <Stack>
+    <Stack className={classes.items}>
       {items.map((item) => (
         <CartItem item={item} />
       ))}
